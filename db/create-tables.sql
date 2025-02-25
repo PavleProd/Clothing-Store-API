0ba3b4001321598e4c6e639475d1a9f130952b03
Павле Продановић <96532387+PavleProd@users.noreply.github.com>
2025-02-25 14:57:13 +0100

    add script for creating tables, add init sample data

---
+++ db/create-tables.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS products;
+DROP TYPE IF EXISTS Size;
+DROP TYPE IF EXISTS Gender;
+
+CREATE TYPE Size AS ENUM ('XS', 'S', 'M', 'L', 'XL', 'XXL');
+CREATE TYPE Gender AS ENUM ('Male', 'Female', 'Unisex');
+
+CREATE TABLE products (
+    id  SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    category VARCHAR(100) NOT NULL,
+    size Size NOT NULL,
+    gender Gender NOT NULL,
+    is_for_kids BOOLEAN NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    quantity INT NOT NULL CHECK (quantity > 0)
+);
--- model/product.go
+++ model/product.go
@@ -1,9 +1,11 @@
 package model
 
 type Product struct {
-	Name     string  `json:"name"`
-	Category string  `json:"category"`
-	Size     string  `json:"size"`
-	Price    float32 `json:"price"`
-	Quantity uint16  `json:"quantity"`
+	Name      string  `json:"name"`
+	Category  string  `json:"category"`
+	Size      string  `json:"size"`
+	Gender    string  `json:"gender"`
+	IsForKids bool    `'json:"isForKids"`
+	Price     float32 `json:"price"`
+	Quantity  uint16  `json:"quantity"`
 }
